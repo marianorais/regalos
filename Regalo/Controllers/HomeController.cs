7f3da2c2a3c8a34d95349e016dc64dc609f58d94
mariano <marianrais13@gmail.com>
2022-12-25 10:34:16 -0300

    proyect

---
+++ Regalo/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Regalo.Models;
+using System.Diagnostics;
+
+namespace Regalo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ConfirmarFoto()
+        {
+            return View();
+        }
+        public IActionResult Page14()
+        {
+            return View();
+        }
+        public IActionResult Page11()
+        {
+            return View();
+        }
+        public IActionResult Page26()
+        {
+            return View();
+        }
+        public IActionResult Page20()
+        {
+            return View();
+        }
+        public IActionResult Page1()
+        {
+            return View();
+        }
+        public IActionResult Page9()
+        {
+            return View();
+        }
+
+        public IActionResult Page4()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
